7ab4d732a76a1c7ad33453fee851af6a62376d93
nahy-512 <hedghog1004@naver.com>
2023-08-19 04:32:45 +0900

    feat: 랭킹 화면 리사이클러뷰 데이터 바인딩 진행중

---
+++ app/src/main/java/com/chikorita/gamagochi/data/RankingData.kt
@@ -0,0 +1,9 @@
+package com.chikorita.gamagochi.data
+
+data class RankingData (
+    val rank: Int,
+    val nickName: String,
+//    val major: String,
+    val experience: Int,
+    val ladybugType: String
+)
