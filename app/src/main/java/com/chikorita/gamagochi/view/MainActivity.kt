f5ad08edd2c49431374156f74f23d0cd500bdcc8
nahy-512 <hedghog1004@naver.com>
2023-08-18 18:07:19 +0900

    feat: 카카오 맵 현재 위치 마커 추가

--- app/src/main/java/com/chikorita/gamagochi/view/MainActivity.kt
+++ app/src/main/java/com/chikorita/gamagochi/view/MainActivity.kt
@@ -11,8 +11,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.chikorita.gamagochi.R
 import com.chikorita.gamagochi.base.BaseActivity
 import com.chikorita.gamagochi.databinding.ActivityMainBinding
+import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
@@ -33,15 +35,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         initMapView()
         getLocationPermission()
         setCurrentLocation()
+        addCustomMarker()
     }
 
     override fun finish() {
         binding.activityMainKakaoMapView.removeView(mapView)
         super.finish()
-    }
-    private fun setCurrentLocation() {
-        val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
-        mapView.setMapCenterPoint(uNowPosition, true)
     }
 
     private fun getLocationPermission() {
@@ -75,5 +74,26 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         mapView = MapView(this)
         mapViewContainer = binding.activityMainKakaoMapView
         mapViewContainer.addView(mapView)
+    }
+
+    private fun setCurrentLocation() {
+        val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
+        mapView.setMapCenterPoint(uNowPosition, true)
+    }
+
+    private fun addCustomMarker() {
+        // 서울시청에 마커 추가
+        val marker = MapPOIItem()
+        marker.apply {
+            itemName = "서울시청"   // 마커 이름
+            mapPoint = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)   // 좌표
+            markerType = MapPOIItem.MarkerType.CustomImage          // 마커 모양 (커스텀)
+            customImageResourceId = R.drawable.ic_custom_marker               // 커스텀 마커 이미지
+            selectedMarkerType = MapPOIItem.MarkerType.CustomImage  // 클릭 시 마커 모양 (커스텀)
+            customSelectedImageResourceId = R.drawable.ic_custom_marker       // 클릭 시 커스텀 마커 이미지
+            isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
+            setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
+        }
+        mapView.addPOIItem(marker)
     }
 }
