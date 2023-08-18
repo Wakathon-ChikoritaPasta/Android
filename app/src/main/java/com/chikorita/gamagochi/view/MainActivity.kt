77dc99f1485dfc3e35b23f65c9220da271da5c38
nahy-512 <hedghog1004@naver.com>
2023-08-18 17:56:33 +0900

    feat: 카카오 맵 추가

--- app/src/main/java/com/chikorita/gamagochi/view/MainActivity.kt
+++ app/src/main/java/com/chikorita/gamagochi/view/MainActivity.kt
@@ -1,24 +1,79 @@
 package com.chikorita.gamagochi.view
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import com.chikorita.gamagochi.R
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.chikorita.gamagochi.base.BaseActivity
-import com.chikorita.gamagochi.databinding.ActivityLoginBinding
 import com.chikorita.gamagochi.databinding.ActivityMainBinding
-import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.model.ClientError
-import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.common.util.Utility
-import com.kakao.sdk.user.UserApiClient
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
+
 
 //MainActivity.kt
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
+
+    lateinit var mapView: MapView
+    private lateinit var mapViewContainer : ViewGroup
+
+
+    private var uLatitude : Double = 0.0
+    private var uLongitude : Double = 0.0
+
+
     override fun initView() {
-        TODO("Not yet implemented")
+
+        initMapView()
+        getLocationPermission()
+        setCurrentLocation()
     }
 
+    override fun finish() {
+        binding.activityMainKakaoMapView.removeView(mapView)
+        super.finish()
+    }
+    private fun setCurrentLocation() {
+        val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
+        mapView.setMapCenterPoint(uNowPosition, true)
+    }
+
+    private fun getLocationPermission() {
+        val permissionCheck = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
+
+        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        try {
+            val userNowLocation : Location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
+            uLatitude = userNowLocation.latitude
+            uLongitude = userNowLocation.longitude
+        } catch (e : NullPointerException) {
+            Log.e("LOCATION_ERROR", e.toString())
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ActivityCompat.finishAffinity(this)
+            } else {
+                ActivityCompat.finishAffinity(this)
+            }
+
+            finish()
+        }
+    }
+
+    private fun initMapView() {
+        mapView = MapView(this)
+        mapViewContainer = binding.activityMainKakaoMapView
+        mapViewContainer.addView(mapView)
+    }
 }
